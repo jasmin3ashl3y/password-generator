e522c98a1f79a8960b7bc0e0542ed2a46d0e1b34
jasmin3ashl3y <jasmine@ashleyit.com>
2021-09-12 17:40:36 -0400

    create prompts and validation

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,39 @@
-// Assignment code here
+// Assignment code here  
 
-function generatePassword(passwordLength) {
-    var numberChars = "0123456789";
-    var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowerChars = "abcdefghijklmnopqrstuvwxyz";
-    var allChars = numberChars + upperChars + lowerChars;
-    var randPasswordArray = Array(passwordLength);
-    randPasswordArray[0] = numberChars;
-    randPasswordArray[1] = upperChars;
-    randPasswordArray[2] = lowerChars;
-    randPasswordArray = randPasswordArray.fill(allChars, 3);
-    return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
-}  
+function generatePassword(passwordLength, allChars){
+    var password = ""
+    i = 1
+    while ( i <= passwordLength ){
+      password += allChars.substr(Math.floor(Math.random() * allChars.length), 1)
+      i++
+    }
+    return password
+}
 
 // Write password to the #password input
-function writePassword(passwordText) {
-    var password = generatePassword("passwordLength");
+function writePassword() {
+    var passwordLength = prompt("Choose a password length between 8 and 128 characters")
+    var affirmative = ["y","Y","yes","YES","Yes"]
+    var allChars = ""
+    while (allChars === ""){ 
+        var useUpper = prompt ("Do you want to use uppercase?")
+        var useLower = prompt ("Do you want to use lowercase?")
+        var useNumbers = prompt ("Do you want to use numbers?")
+        var useSpecial = ("Do you want to use special characters?") 
+        if (affirmative.includes(useUpper)){
+            allChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        }
+        if (affirmative.includes(useLower)){
+            allChars += "abcdefghijklmnopqrstuvwxyz"
+        }
+        if (affirmative.includes(useNumbers)){
+            allChars += "0123456789"
+        }
+        if (affirmative.includes(useSpecial)){
+            allChars += "!@#$%^&*()-_+:;,.<>?[]{}|/"
+        }
+    }
+    var password = generatePassword(passwordLength, allChars);
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
     console.log (generate.password);
@@ -31,11 +49,13 @@ function shuffleArray(array) {
     return array;
   }
     
-  alert(generatePassword(12));
-
 // Get references to the #generate element
-  var generateBtn = document.querySelector("#card-body");
+generatePassword(0);
+    
+  var generateBtn = document.querySelector("#generate");
+  var writePassword = generateBtn.addEventListener("click", writePassword);
 
 // Add event listener to generate button
 
 generateBtn.addEventListener("click", writePassword);
+    
